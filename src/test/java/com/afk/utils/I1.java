5df1288fe11a0683d976468fa3b346ea492d7d7f
Abdul-Firoz-Khan <165898558+Abdul-Firoz-Khan@users.noreply.github.com>
2024-07-18 22:12:49 +0530

    Update I1.java

--- src/test/java/com/afk/utils/I1.java
+++ src/test/java/com/afk/utils/I1.java
@@ -3,7 +3,8 @@ package com.afk.utils;
 import org.openqa.selenium.By;
 
 public class I1 {
-    protected String base_url ="https://www.amazon.in/";
+    protected String base_url = "https://www.amazon.in/";
     protected By signIn = By.xpath("//span[@id='nav-link-accountList-nav-line-1']");
-    protected By  createAccount = By.xpath("//a[@id='createAccountSubmit']");
+    protected By createAccount = By.xpath("//a[@id='createAccountSubmit']");
+    protected By conditionsOfUse = By.xpath("//a[contains(text(),'Conditions of Use')]");
 }
